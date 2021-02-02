046724368d556c5af0465ee3974718f6c57e5f14
eugeen3 <-v>
2021-02-02 21:39:08 +0300

    converted to Kotlin ViewModel, MainActivity, Repository and Room classes

---
+++ app/src/main/java/eugeen3/keepinfit/ui/FoodItemsFragment.kt
@@ -0,0 +1,12 @@
+package eugeen3.keepinfit.ui
+
+import eugeen3.keepinfit.R
+import eugeen3.keepinfit.databinding.FragmentFoodItemsBinding
+
+class FoodItemsFragment : BaseFragment(R.layout.fragment_food_items) {
+    private lateinit var mBinding: FragmentFoodItemsBinding
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
