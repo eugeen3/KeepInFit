0cb10dccfe815ec652f493794bf54aa375135987
eugeen3 <-v>
2021-02-07 15:42:08 +0300

    added validation to FoodItemFragment.kt

--- app/src/main/java/eugeen3/keepinfit/ui/FoodItemFragment.kt
+++ app/src/main/java/eugeen3/keepinfit/ui/FoodItemFragment.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import eugeen3.keepinfit.R
 import eugeen3.keepinfit.databinding.FragmentFoodItemBinding
@@ -13,6 +14,7 @@ import eugeen3.keepinfit.entities.FoodItem
 import eugeen3.keepinfit.viewmodels.SharedViewModel
 import java.lang.Float.parseFloat
 import java.lang.Integer.parseInt
+
 
 class FoodItemFragment : BaseFragment(R.layout.fragment_food_item) {
     private var mBinding: FragmentFoodItemBinding? = null
@@ -23,7 +25,7 @@ class FoodItemFragment : BaseFragment(R.layout.fragment_food_item) {
         val binding = FragmentFoodItemBinding.bind(view)
         mBinding = binding
         setHasOptionsMenu(true)
-        val actionBar : ActionBar? = activity?.actionBar
+        val actionBar: ActionBar? = activity?.actionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
         actionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
         actionBar?.title = getString(R.string.FoodItemFragmentTitle)
@@ -46,23 +48,135 @@ class FoodItemFragment : BaseFragment(R.layout.fragment_food_item) {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.save_food_item_in_db -> {
-                sharedViewModel.insert(createFoodItem())
-                fragmentManager
-                        ?.beginTransaction()
-                        ?.replace(R.id.vFragmentContainer,FoodItemsListFragment())
-                        ?.commit()
+                val newFoodItem: FoodItem? = createFoodItem()
+                if (newFoodItem != null) {
+                    sharedViewModel.insert(newFoodItem)
+                    fragmentManager
+                            ?.beginTransaction()
+                            ?.replace(R.id.vFragmentContainer, FoodItemsListFragment())
+                            ?.commit()
+                }
             }
         }
         return true
     }
 
-    fun createFoodItem() : FoodItem{
-        return FoodItem(
-                mBinding?.etFoodItemTitle?.text.toString(),
-                parseFloat(mBinding?.etFoodItemProteins?.text.toString()),
-                parseFloat(mBinding?.etFoodItemFats?.text.toString()),
-                parseFloat(mBinding?.etFoodItemCarbohydrates?.text.toString()),
-                parseInt(mBinding?.etFoodItemKilocalories?.text.toString())
-        )
+    private fun createFoodItem(): FoodItem? {
+        return if (validateAll())
+            FoodItem(
+                    mBinding?.etFoodItemTitle?.text.toString(),
+                    parseFloat(mBinding?.etFoodItemProteins?.text.toString()),
+                    parseFloat(mBinding?.etFoodItemFats?.text.toString()),
+                    parseFloat(mBinding?.etFoodItemCarbohydrates?.text.toString()),
+                    parseInt(mBinding?.etFoodItemKilocalories?.text.toString())
+            )
+        else {
+            null
+        }
+    }
+
+    private fun validateAll(): Boolean {
+        return if (validateTitle() &&
+                validateProteins() &&
+                validateFats() &&
+                validateCarbohydrates() &&
+                validateKilocalories()) {
+            if (parseFloat(mBinding?.etFoodItemProteins?.text.toString()) +
+                    parseFloat(mBinding?.etFoodItemFats?.text.toString()) +
+                    parseFloat(mBinding?.etFoodItemCarbohydrates?.text.toString()) > 100) {
+                Toast.makeText(activity,
+                        getString(R.string.toastCheckIfDataCorrect), Toast.LENGTH_SHORT).show()
+                false
+            } else true
+        } else {
+            false
+        }
+    }
+
+    private fun validateTitle(): Boolean {
+        return when {
+            mBinding?.etFoodItemTitle?.text?.isEmpty() == true -> {
+                mBinding?.etFoodItemTitle?.error =
+                        getString(R.string.etEmptyTitleError)
+                false
+            }
+            else -> true
+        }
+    }
+
+    private fun validateProteins(): Boolean {
+        return when {
+            mBinding?.etFoodItemProteins?.text?.isEmpty() == true -> {
+                mBinding?.etFoodItemProteins?.error =
+                        getString(R.string.etEmptyNumberError)
+                false
+            }
+            parseFloat(mBinding?.etFoodItemProteins?.text.toString()) > 100 -> {
+                mBinding?.etFoodItemProteins?.error =
+                        getString(R.string.etInputNumberWrongValueError)
+                false
+            }
+            else -> {
+                mBinding?.etFoodItemProteins?.error = null
+                true
+            }
+        }
+    }
+
+    private fun validateFats(): Boolean {
+        return when {
+            mBinding?.etFoodItemFats?.text?.isEmpty() == true -> {
+                mBinding?.etFoodItemFats?.error =
+                        getString(R.string.etEmptyNumberError)
+                false
+            }
+            parseFloat(mBinding?.etFoodItemFats?.text.toString()) > 100 -> {
+                mBinding?.etFoodItemFats?.error =
+                        getString(R.string.etInputNumberWrongValueError)
+                false
+            }
+            else -> {
+                mBinding?.etFoodItemFats?.error = null
+                true
+            }
+        }
+    }
+
+    private fun validateCarbohydrates(): Boolean {
+        return when {
+            mBinding?.etFoodItemCarbohydrates?.text?.isEmpty() == true -> {
+                mBinding?.etFoodItemCarbohydrates?.error =
+                        getString(R.string.etEmptyNumberError)
+                false
+            }
+            parseFloat(mBinding?.etFoodItemCarbohydrates?.text.toString()) > 100 -> {
+                mBinding?.etFoodItemCarbohydrates?.error =
+                        getString(R.string.etInputNumberWrongValueError)
+                false
+            }
+            else -> {
+                mBinding?.etFoodItemCarbohydrates?.error = null
+                true
+            }
+        }
+    }
+
+    private fun validateKilocalories(): Boolean {
+        return when {
+            mBinding?.etFoodItemKilocalories?.text?.isEmpty() == true -> {
+                mBinding?.etFoodItemKilocalories?.error =
+                        getString(R.string.etEmptyNumberError)
+                false
+            }
+            parseInt(mBinding?.etFoodItemKilocalories?.text.toString()) >= 1000 -> {
+                mBinding?.etFoodItemKilocalories?.error =
+                        getString(R.string.etInputNumberWrongValueKilocaloriesError)
+                false
+            }
+            else -> {
+                mBinding?.etFoodItemKilocalories?.error = null
+                true
+            }
+        }
     }
 }
